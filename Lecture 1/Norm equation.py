743b9a0b5615d9af9bea9038b9260c01161d0aba
MaxThFe <mftheisen@outlook.de>
2024-02-05 08:48:17 +0100

    fix bug due to hidden tokens

--- Lecture 1/Norm equation.py
+++ Lecture 1/Norm equation.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Generate some example data
-X = np.linspace(0, 10, 100)  # Input features (in this case, a single feature)
-y = 3* X + 2 + np.random.normal(size=X.size)  # Target variable
+X = np.linspace(0, 10, 100) # Input features (in this case, a single feature)
+y = 3* X + 2 + np.random.normal(size=X.size) # Target variable
 
 # Add a column of ones to X for the bias term (intercept)
 X_b = np.c_[np.ones((X.shape[0], 1)), X]
@@ -13,12 +13,16 @@ theta = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y)
 
 # Predict values using the calculated coefficients
 X_test = np.linspace(0, 10, 100).reshape(-1, 1)
-X_test_b = np.c_[np.ones((X_test.shape[0], 1)), X_test]y_pred = X_test_b.dot(theta)
+X_test_b = np.c_[np.ones((X_test.shape[0], 1)), X_test]
+y_pred = X_test_b.dot(theta)
 
 # Plot the original data points and the regression line
 plt.scatter(X, y, label="Training Data", color="blue")
 plt.plot(X_test, y_pred, label="Regression Line", color="red")
-plt.xlabel("X")plt.ylabel("y")plt.legend()plt.show()
+plt.xlabel("X")
+plt.ylabel("y")
+plt.legend()
+plt.show()
 
 # Print the calculated coefficients (intercept and slope)
 print("Intercept:", theta[0])
--- Lecture 1/Polynomial regression.py
+++ Lecture 1/Polynomial regression.py
@@ -5,17 +5,18 @@ from sklearn.preprocessing import PolynomialFeatures
 np.random.seed(3)
 # Generating the curve
 x = np.linspace(0, 1, 400)
-y = np.sin(2*np.pi * x)# Generating and plotting the blue circles
+y = np.sin(2*np.pi * x) # Generating and plotting the blue circles
 x_points = np.linspace(0, 1, 10)
 y_points = np.sin(2*np.pi * x_points) + np.random.normal(0,0.2,10)
-plt.scatter(x_points, y_points, color='blue', edgecolors='black')polynomial_features = PolynomialFeatures(degree=3)
+plt.scatter(x_points, y_points, color='blue', edgecolors='black')
+polynomial_features = PolynomialFeatures(degree=3)
 x_poly_train = polynomial_features.fit_transform(x_points.reshape(-1, 1))
 x_poly_test = polynomial_features.transform(x.reshape(-1, 1))
 # Training the model
-model = LinearRegression().fit(x_poly_train, y_points)# Predicting
-y_poly_pred_test = model.predict(x_poly_test)# Setting axis labels
+model = LinearRegression().fit(x_poly_train, y_points) # Predicting
+y_poly_pred_test = model.predict(x_poly_test) # Setting axis labels
 plt.xlabel('x')
-plt.ylabel('t')# Displaying the grid
+plt.ylabel('t') # Displaying the grid
 plt.grid(True, which='both', linestyle='--', linewidth=0.5)
 plt.axhline(y=0, color='k', linewidth=0.8)
 plt.axvline(x=0, color='k', linewidth=0.8)
