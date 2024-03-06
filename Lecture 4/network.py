e318573699e3b5dd365381568b53f2d68e42b63f
Tanuj Karia <tkaria@tudelft.nl>
2024-03-06 11:39:44 +0100

    Added live coding notebooks for L4

---
+++ Lecture 4/network.py
@@ -0,0 +1,102 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as thdat
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def np_to_th(x):
+    n_samples = len(x)
+    return torch.from_numpy(x).to(torch.float).to(DEVICE).reshape(n_samples, -1)
+
+
+class Net(nn.Module):
+    def __init__(
+        self,
+        input_dim,
+        output_dim,
+        n_units=100,
+        epochs=1000,
+        loss=nn.MSELoss(),
+        lr=1e-3,
+        loss2=None,
+        loss2_weight=0.1,
+    ) -> None:
+        super().__init__()
+
+        self.epochs = epochs
+        self.loss = loss
+        self.loss2 = loss2
+        self.loss2_weight = loss2_weight
+        self.lr = lr
+        self.n_units = n_units
+
+        self.layers = nn.Sequential(
+            nn.Linear(input_dim, self.n_units),
+            nn.ReLU(),
+            nn.Linear(self.n_units, self.n_units),
+            nn.ReLU(),
+            nn.Linear(self.n_units, self.n_units),
+            nn.ReLU(),
+            nn.Linear(self.n_units, self.n_units),
+            nn.ReLU(),
+        )
+        self.out = nn.Linear(self.n_units, output_dim)
+
+    def forward(self, x):
+        h = self.layers(x)
+        out = self.out(h)
+
+        return out
+
+    def fit(self, X, y):
+        Xt = np_to_th(X)
+        yt = np_to_th(y)
+
+        optimiser = optim.Adam(self.parameters(), lr=self.lr)
+        self.train()
+        losses = []
+        for ep in range(self.epochs):
+            optimiser.zero_grad()
+            outputs = self.forward(Xt)
+            loss = self.loss(yt, outputs)
+            og_loss = self.loss(yt, outputs)
+            if self.loss2:
+                second_loss_val = self.loss2_weight * self.loss2(self)
+#                loss += self.loss2_weight + self.loss2_weight * self.loss2(self)
+                loss += second_loss_val
+            loss.backward()
+            optimiser.step()
+            losses.append(loss.item())
+            if self.loss2:
+                if ep % int(self.epochs / 10) == 0:
+                    print(f"Epoch {ep}/{self.epochs}, original loss: {og_loss:.2f}, second loss: {second_loss_val:.2f}, total loss: {losses[-1]:.2f}")
+            else:
+                if ep % int(self.epochs / 10) == 0:
+                    print(f"Epoch {ep}/{self.epochs}, loss: {losses[-1]:.2f}")
+        return losses
+
+    def predict(self, X):
+        self.eval()
+        out = self.forward(np_to_th(X))
+        return out.detach().cpu().numpy()
+
+
+class NetDiscovery(Net):
+    def __init__(
+        self,
+        input_dim,
+        output_dim,
+        n_units=100,
+        epochs=1000,
+        loss=nn.MSELoss(),
+        lr=0.001,
+        loss2=None,
+        loss2_weight=0.1,
+    ) -> None:
+        super().__init__(
+            input_dim, output_dim, n_units, epochs, loss, lr, loss2, loss2_weight
+        )
+
+        self.r = nn.Parameter(data=torch.tensor([0.]))
---
+++ Lecture 4/readme.txt
@@ -0,0 +1,3 @@
+1. For the first case study, the model giving the perfect extrapolation is provided in the pth file in the Pytorch model folder
+2. The file network.py is needed to run the notebook L4_CS2_PINN
+3. The pth file is the Pytorch model with excellent extrapolation performance as shown in the lecture slides
